8f68b1a3cc5e3798541c33f48dd3bf83c1d283ad
HRYUN <yunhyerim41@gmail.com>
2022-09-11 04:01:24 +0900

    Initial commit

---
+++ src/main/java/com/prac/accesstoken_refreshtoken_with_redis/AccesstokenRefreshtokenWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.prac.accesstoken_refreshtoken_with_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesstokenRefreshtokenWithRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccesstokenRefreshtokenWithRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prac/accesstoken_refreshtoken_with_redis/AccesstokenRefreshtokenWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.accesstoken_refreshtoken_with_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesstokenRefreshtokenWithRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
